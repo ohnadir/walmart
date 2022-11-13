0c26d526fb18ed2963dad36df2e25ba56e3e03f6
Nadir Ahmed <qx.nadir@gmail.com>
2022-11-13 17:24:11 +0600

    drawer implement

--- client/src/Component/Shared/Navbar.jsx
+++ client/src/Component/Shared/Navbar.jsx
@@ -2,20 +2,56 @@ import React from 'react'
 import '../../Style/Navbar.css';
 import logo from '../../Assets/walmartlogo.png'
 import { MdOutlineGridView } from 'react-icons/md';
-import { BsGridFill, BsCart } from 'react-icons/bs';
+import { BsGridFill, BsCart, BsChat } from 'react-icons/bs';
 import { IoIosSearch } from 'react-icons/io';
 import { FiHeart } from 'react-icons/fi';
 import { CiUser } from 'react-icons/ci';
 import { GrMenu } from 'react-icons/gr';
+import { BiReceipt } from 'react-icons/bi';
+import { TbDownload } from 'react-icons/tb';
+import { TfiHelpAlt } from 'react-icons/tfi';
+import { GoGift } from 'react-icons/go';
 
 const Navbar = () => {
+  const openNav=()=>{
+    document.getElementById("mySideNav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    document.getElementById("btn").style.display= "block";
+  }
+  const closeNav=()=>{
+    document.getElementById("mySideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("btn").style.display= "none";
+  }
   return (
-    <div className='navContainer'>
+    <>
+      <div id="mySideNav" class="sideNav">
+        <div className='flex items-center gap-5 p-5 sideNavAuth'>
+          <img onClick={closeNav} className='w-[28px] cursor-pointer' src={logo} alt="" />
+          <button id='btn'>Sign in or create account</button>
+        </div>
+        <div className='p-5'>
+          <ul>
+            <li><BiReceipt/> Purchase History</li>
+            <li><TbDownload/> My Items</li>
+            <li><CiUser/> Account</li>
+            <li className='blind'><TfiHelpAlt/> Help</li>
+            <li><FiHeart/> Lists</li>
+            <li><GoGift/> Registries</li>
+            <div className='blind'>
+              <li><MdOutlineGridView/> Departments</li>
+              <li><BsGridFill/> Services</li>
+            </div>
+            <li><BsChat/> Give Feedback</li>
+          </ul>
+        </div>
+      </div>
+    <div className='navContainer' id="main">
       <div className='flex justify-between items-center gap-10'>
         <div className=' flex items-center  gap-10'>
             <div className='block lg:hidden'>
               <div className='flex items-center gap-10'>
-                <GrMenu className='text-[55px] cursor-pointer'/>
+                <GrMenu onClick={openNav}  className='menuBtn'/>
                 <img className='w-[32px]' src={logo} alt="" />
               </div>
             </div>
@@ -71,6 +107,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
+    </>
   )
 }
 
