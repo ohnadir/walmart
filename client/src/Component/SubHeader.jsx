7460d2df104a19e97892f52e70d4997a0dfc94e6
Nadir Ahmed <qx.nadir@gmail.com>
2022-11-22 09:59:51 +0600

    subHeader something updated

--- client/src/Component/SubHeader.jsx
+++ client/src/Component/SubHeader.jsx
@@ -11,7 +11,7 @@ import delivery from '../Assets/delivary.png'
 const SubHeader = () => {
   const [card, setCard] = useState(false)
   return (
-    <div className='subHeader'>
+    <div className='subHeader '>
         <div className='flex items-center gap-2'>
             <div className='flex items-center gap-2 cursor-pointer' onClick={()=>setCard(!card)}>
               <img className='w-[30px]' src={phone} alt="" />
@@ -34,49 +34,54 @@ const SubHeader = () => {
         </div>
         {
           card && 
-          <div className='card'>
-            <div className='flex items-center justify-center gap-7'>
-              <div className="optionItem">
-                <img className='mx-auto' src={shipping} alt="" />
-                <p className='mt-[10px]'>Shipping</p>
-              </div>
-              <div className="optionItem">
-                <img className='mx-auto' src={pickup} alt="" />
-                <p className='mt-[10px]'>Pickup</p>
-              </div>
-              <div className="optionItem">
-                <img className='mx-auto' src={delivery} alt="" />
-                <p className='mt-[10px]'>Delivery</p>
-              </div>
-            </div>
-            <div className='cardItem my-5'>
-              <div className='flex  gap-2'>
-                <MdOutlineLocationOn className='icon'/>
-                <div className='mt-[-5px]'>
-                  <h6 >Add an address for shipping and delivery</h6>
-                  <p className='m-0 text-xs'>Sacramento, CA 95829</p>
+          <div className=''>
+            {
+              card ? <div className=' w-full absolute left-0 z-10 bg-blue-300 cardOverlay'></div> : ""
+            }
+            <div className='card'>
+              <div className='flex items-center justify-center gap-7'>
+                <div className="optionItem">
+                  <img className='mx-auto' src={shipping} alt="" />
+                  <p className='mt-[10px]'>Shipping</p>
+                </div>
+                <div className="optionItem">
+                  <img className='mx-auto' src={pickup} alt="" />
+                  <p className='mt-[10px]'>Pickup</p>
+                </div>
+                <div className="optionItem">
+                  <img className='mx-auto' src={delivery} alt="" />
+                  <p className='mt-[10px]'>Delivery</p>
                 </div>
               </div>
-            </div>
-            <div className='cardItem'>
-              <div className='flex  gap-2'>
-                <SlHome className='icon' />
-                <div className='mt-[-5px]'>
-                  <h6 >Add an address for shipping and delivery</h6>
-                  <p className='m-0 text-xs flex items-center gap-1'>
-                    8915 Gerber Road, Sacramento, CA 95829 
-                    <span><MdArrowForwardIos className='text-lg' /></span>
-                  </p>
-                  <ul className='flex items-center justify-between mt-[6px]'>
-                    <li>
-                      <TiTickOutline/>
-                      Curbside pickup
-                    </li>
-                    <li>
-                      <TiTickOutline/>
-                      In-store pickup
-                    </li>
-                  </ul>
+              <div className='cardItem my-5'>
+                <div className='flex  gap-2'>
+                  <MdOutlineLocationOn className='icon'/>
+                  <div className='mt-[-5px]'>
+                    <h6 >Add an address for shipping and delivery</h6>
+                    <p className='m-0 text-xs'>Sacramento, CA 95829</p>
+                  </div>
+                </div>
+              </div>
+              <div className='cardItem'>
+                <div className='flex  gap-2'>
+                  <SlHome className='icon' />
+                  <div className='mt-[-5px]'>
+                    <h6 >Add an address for shipping and delivery</h6>
+                    <p className='m-0 text-xs flex items-center gap-1'>
+                      8915 Gerber Road, Sacramento, CA 95829 
+                      <span><MdArrowForwardIos className='text-lg' /></span>
+                    </p>
+                    <ul className='flex items-center justify-between mt-[6px]'>
+                      <li>
+                        <TiTickOutline/>
+                        Curbside pickup
+                      </li>
+                      <li>
+                        <TiTickOutline/>
+                        In-store pickup
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
